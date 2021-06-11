183ce12244d4d84c1d87dcc0107bcb0cb14245e1
Jose Gomez <GomezLopezJose145@gmail.com>
2021-06-10 19:41:40 -0500

    Commit inicial: Codigo base + readme

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# TODOList
+
+El presente proyecto toma como base el codigo desarrollado por el usuario: **sefyudem**, 
+el codigo se puede localizar en su repositorio [Javascript-Todo-App](https://github.com/sefyudem/Javascript-Todo-App).
+
+## Descripcion:
+TODOList basico para listar tareas pendientes por realizar.
+
+## Mejoras por realizar
+Agregar un temporizador para asignar tiempo a cada tarea.
+
---
+++ main.js
@@ -0,0 +1,89 @@
+const add = document.querySelector('.add');
+var input = document.querySelector('.input');
+const container = document.querySelector('.container');
+
+
+if(window.localStorage.getItem('tasks') == undefined){
+    var tasks = [];
+    window.localStorage.setItem('tasks', JSON.stringify(tasks));
+}else{
+    var alltasks = window.localStorage.getItem('tasks');
+    tasks = JSON.parse(alltasks);
+}
+
+
+class item{
+    constructor(itemName){
+        this.createDiv(itemName);
+    }
+
+    createDiv(itemName){
+        let input = document.createElement('input');
+        input.value = itemName;
+        input.disabled = true;
+        input.classList.add('item_input');
+        input.type = "text";
+
+        let itemBox = document.createElement('div');
+        itemBox.classList.add('item');
+
+        let editButton = document.createElement('button');
+        editButton.innerHTML = "Edit";
+        editButton.classList.add('editButton');
+
+        let removeButton = document.createElement('button');
+        removeButton.innerHTML = "Remove";
+        removeButton.classList.add('removeButton');
+
+        container.appendChild(itemBox);
+        itemBox.appendChild(input);
+        itemBox.appendChild(editButton);
+        itemBox.appendChild(removeButton);
+        editButton.addEventListener('click', ()=> this.edit(input, editButton, itemName));
+        removeButton.addEventListener('click', ()=> this.remove(itemBox, itemName));
+    }
+
+    edit(input, editButton, itemName){
+        if(input.disabled == true){
+            input.disabled = false;
+            input.focus();
+            editButton.innerHTML = "Save";
+        }else{
+            input.disabled = true;
+            editButton.innerHTML = "Edit";
+            let index = tasks.indexOf(itemName);
+            tasks[index] = input.value;
+            window.localStorage.setItem('tasks', JSON.stringify(tasks));
+        }
+
+    }
+
+    remove(item, itemName){
+        container.removeChild(item);
+        let index = tasks.indexOf(itemName);
+        tasks.splice(index, 1);
+        window.localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+}
+
+function check(){
+    if(input.value != ""){
+        new item(input.value);
+        tasks.push(input.value);
+        window.localStorage.setItem('tasks', JSON.stringify(tasks));
+        input.value = "";
+    }else{
+        alert('Introduce una tarea');
+    }
+}
+add.addEventListener('click', check);
+
+window.addEventListener('keydown', (e)=> {
+    if(e.which == 13){
+        check();
+    }
+});
+
+for (var i= 0 ; i < tasks.length ; i++){
+    new item(tasks[i]);
+}
